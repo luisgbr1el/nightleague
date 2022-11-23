6ccd2e8caec397ba7c1718261094351b6d5cb9ff
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2022-11-23 13:38:43 +0000

    Creating "scheduled" page

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import App from './App'
 import Teams from './components/Teams'
 import Team from './components/Team'
+import Scheduled from './components/Scheduled'
 
 import {
   Routes as RoutesA, Route, BrowserRouter as Router } from "react-router-dom";
@@ -12,6 +13,7 @@ function Routes() {
         <Route path="/" element={<App />} />
         <Route path="/teams" element={<Teams />} />
         <Route path="/team/:id" element={<Team />} />
+        <Route path="/scheduled" element={<Scheduled />} />
       </RoutesA>
     </Router>
   )
